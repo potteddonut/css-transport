e137b3a8826e8169bf15f7fd671f3a979ca3cbcb
zach <s10241816@connect.np.edu.sg>
2024-01-19 16:58:10 +0800

    Create navbar

---
+++ css-transport/src/app/ui/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <Link className={styles.navitem} href="/">Home</Link>
+            <Link className={styles.navitem} href="/">Bus Arrival Information</Link>
+            <Link className={styles.navitem} href="/">Realtime traffic map</Link>
+            <Link className={styles.navitem} href="/">Parking space availability map</Link>
+        </nav>
+    );
+}
