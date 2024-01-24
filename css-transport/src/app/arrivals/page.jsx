30e13b5c12f0b6cba83726d68135805ef5d33d78
zach <s10241816@connect.np.edu.sg>
2024-01-24 20:03:26 +0800

    Init arrivals page

---
+++ css-transport/src/app/arrivals/page.jsx
@@ -0,0 +1,5 @@
+export default function Arrivals() {
+    return (
+        <p>hello</p>
+    )
+}
--- css-transport/src/app/ui/Navbar.jsx
+++ css-transport/src/app/ui/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <Link className={styles.navitem} href="/">Home</Link>
-            <Link className={styles.navitem} href="/">Bus Arrival Information</Link>
+            <Link className={styles.navitem} href="./arrivals">Bus Arrival Information</Link>
             <Link className={styles.navitem} href="/">Realtime traffic map</Link>
             <Link className={styles.navitem} href="/">Parking space availability map</Link>
             <Link className={styles.navitem} href="./causeway">Causeway Traffic Feed</Link>
